9e1903e867dc87e0e1a761e1af4a9c1ef900bbb2
wrryy <woj.rydzewski@gmail.com>
2018-10-28 11:21:18 +0100

    decreased dependency level between two services

--- src/main/java/pl/wrydz/sklep/service/BasketItemService.java
+++ src/main/java/pl/wrydz/sklep/service/BasketItemService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import pl.wrydz.sklep.entity.Basket;
 import pl.wrydz.sklep.entity.BasketItem;
 import pl.wrydz.sklep.repository.BasketItemRepo;
+import pl.wrydz.sklep.repository.BasketRepo;
 
 import java.util.List;
 
@@ -13,17 +14,17 @@ public class BasketItemService {
 
     private BasketItemRepo basketItemRepo;
     private ProductService productService;
-    private BasketService basketService;
+    private BasketRepo basketRepo;
 
     @Autowired
-    public BasketItemService(BasketItemRepo basketItemRepo, ProductService productService, BasketService basketService) {
+    public BasketItemService(BasketItemRepo basketItemRepo, BasketRepo basketRepo, ProductService productService) {
         this.basketItemRepo = basketItemRepo;
+        this.basketRepo = basketRepo;
         this.productService = productService;
-        this.basketService = basketService;
     }
 
     public BasketItem addToBasket(long userId, long productId) {
-        Basket basket = basketService.findBasketByUser(userId);
+        Basket basket = basketRepo.findBasketByUser(userId);
         BasketItem item = getItemFromBasket(basket.getId(), productId);
         if (item == null) {
             item = new BasketItem();
@@ -38,7 +39,7 @@ public class BasketItemService {
     }
 
     public BasketItem removeFromBasket(long userId, long productId) {
-        Basket basket = basketService.findBasketByUser(userId);
+        Basket basket = basketRepo.findBasketByUser(userId);
         BasketItem item = getItemFromBasket(basket.getId(), productId);
         if (item != null) {
             if (item.getQuantity() == 1) {
@@ -55,9 +56,12 @@ public class BasketItemService {
         return basketItemRepo.getItemFromBasket(basketId, productId);
     }
 
-    public double getTotalPrice(long basketId) {
+    double getBasketTotalPrice(long basketId) {
         List<BasketItem> items = basketItemRepo.findBasketItemsByBasket(basketId);
         return items.stream().mapToDouble(BasketItem::getTotalPrice).sum();
     }
+//    double calculatePrice(BasketItem item){
+//
+//    }
 
 }
--- src/main/java/pl/wrydz/sklep/service/BasketService.java
+++ src/main/java/pl/wrydz/sklep/service/BasketService.java
@@ -8,34 +8,33 @@ import pl.wrydz.sklep.repository.BasketRepo;
 @Service
 public class BasketService {
 
-    private UserService userService;
     private BasketRepo basketRepo;
+    private UserService userService;
+    private BasketItemService basketItemService;
 
     @Autowired
-    public BasketService(UserService userService, BasketRepo basketRepo) {
-        this.userService = userService;
+    public BasketService(BasketRepo basketRepo, UserService userService, BasketItemService basketItemService) {
         this.basketRepo = basketRepo;
+        this.userService = userService;
+        this.basketItemService = basketItemService;
     }
 
     public Basket getBasket(long userId) {
-        Basket basket = findBasketByUser(userId);
+        Basket basket = basketRepo.findBasketByUser(userId);
         if (basket == null) {
             basket = new Basket();
             basket.setUser(userService.getUser(userId));
-            basketRepo.saveAndFlush(basket);
+            basketRepo.save(basket);
         }
         return basket;
     }
 
     public Basket checkoutBasket(long userId) {
-        Basket basket = findBasketByUser(userId);
+        Basket basket = basketRepo.findBasketByUser(userId);
         basket.setClosed(true);
+        basket.setBasketPrice(basketItemService.getBasketTotalPrice(basket.getId()));
         basketRepo.save(basket);
         return basket;
-    }
-
-    public Basket findBasketByUser(long userId) {
-        return basketRepo.findBasketByUser(userId);
     }
 
 }
