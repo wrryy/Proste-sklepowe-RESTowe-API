7e6b3f9c439092f2329187675158be601213eb7a
wrryy <woj.rydzewski@gmail.com>
2018-10-28 08:03:33 +0100

    small changes

--- src/main/java/pl/wrydz/sklep/SklepApplication.java
+++ src/main/java/pl/wrydz/sklep/SklepApplication.java
@@ -3,12 +3,7 @@ package pl.wrydz.sklep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-
-import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication(scanBasePackages = "pl.wrydz.sklep")
 @EnableJpaRepositories
